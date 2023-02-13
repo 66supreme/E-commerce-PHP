9995a29270895f0b9e8d15c5ba16ff92ca2c463f
FOFANA <fof.peralta6@gmail.com>
2023-02-13 08:04:21 +0000

    Modification des fichiers single.php et footer.php

--- shopping/single.php
+++ shopping/single.php
@@ -1,19 +1,33 @@
 <?php require('../includes/header.php'); ?>
 <?php require('../config/config.php'); ?>
 
-<?php 
+<?php
 
-    if(isset($_GET['id'])){
-        $id = $_GET['id'];
+if(isset($_POST['submit'])){
 
-        $row = $conn->query("SELECT * FROM products WHERE status = 1 AND id = '$id'");
-        $row->execute();
+    $pro_id = $_POST['pro_id'];
+    $pro_name = $_POST['pro_name'];
+    $pro_image = $_POST['pro_image'];
+    $pro_price = $_POST['pro_price'];
+    $pro_amount = $_POST['pro_amount'];
+    $pro_file = $_POST['pro_file'];
+    $user_id = $_POST['user_id'];
 
-        $product = $row->fetch(PDO::FETCH_OBJ);
+    $insert = $conn->query("");
+}
 
-    }else{
-        echo "404";
-    }
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $row = $conn->query("SELECT * FROM products WHERE status = 1 AND id = '$id'");
+    $row->execute();
+
+    $product = $row->fetch(PDO::FETCH_OBJ);
+
+} else {
+    echo "404";
+}
 
 ?>
 
@@ -29,7 +43,7 @@
                 <div class="col-md-6">
                     <div class="product p-4">
                         <div class="d-flex justify-content-between align-items-center">
-                            <div class="d-flex align-items-center"> <a href="<?php echo bookstore ; ?>" class="ml-1 btn btn-primary"><i class="fa fa-long-arrow-left"></i> Retour</a> </div> <i class="fa fa-shopping-cart text-muted"></i>
+                            <div class="d-flex align-items-center"> <a href="<?php echo bookstore; ?>" class="ml-1 btn btn-primary"><i class="fa fa-long-arrow-left"></i> Retour</a> </div> <i class="fa fa-shopping-cart text-muted"></i>
                         </div>
                         <div class="mt-4 mb-3">
                             <h5 class="text-uppercase"><?php echo $product->name; ?></h5>
@@ -38,7 +52,31 @@
                         </div>
                         <p class="about"><?php echo $product->description; ?></p>
 
-                        <div class="cart mt-4 align-items-center"> <button class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Ajouter au panier</button> </div>
+                        <form action="" method="post" id="form-data">
+                            <div class="">
+                                <input type="text" name="pro_id" class="form-control" value="<?php echo $product->id; ?>" name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="pro_name" class="form-control" value="<?php echo $product->name;?>" name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="pro_image" class="form-control" value="<?php echo $product->image;?>"  name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="pro_price" class="form-control" value="<?php echo $product->price;?>"  name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="pro_amount" class="form-control" value="1" name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="pro_file" class="form-control" value="<?php echo $product->file;?>"  name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" name="user_id" class="form-control" value="<?php echo $_SESSION['user_id'];?>" name="email">
+                            </div>
+                        </form>
+
+                        <div class="cart mt-4 align-items-center"> <button type="submit" name="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Ajouter au panier</button> </div>
                     </div>
                 </div>
             </div>
@@ -46,3 +84,11 @@
     </div>
 </div>
 <?php require('../includes/footer.php'); ?>
+
+<script>
+$(document).ready(function(){
+    $(document).on("submit",function(e){
+        var $formdata = $("#form-data").serialize()+"&submit=submit";
+    });
+});
+</script>
