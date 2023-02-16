577538bab946fb86cbd0f8c29a6de7c0aa3992df
FOFANA <fof.peralta6@gmail.com>
2023-02-16 14:36:18 +0000

    M

--- shopping/single.php
+++ shopping/single.php
@@ -14,15 +14,15 @@ if (isset($_POST['submit'])) {
     $user_id = $_POST['user_id'];
 
     $insert = $conn->prepare("INSERT INTO panier (pro_id, pro_name, pro_image, pro_price, pro_amount, pro_file, user_id) VALUES (:pro_id, :pro_name, :pro_image, :pro_price, :pro_amount, :pro_file, :user_id)");
-        $insert->execute([
-            ':pro_id'    => $pro_id,
-            ':pro_name'       => $pro_name,
-            ':pro_image'  => $pro_image,
-            ':pro_price'  => $pro_price,
-            ':pro_amount'  => $pro_amount,
-            ':pro_file'  => $pro_file,
-            ':user_id'  => $user_id,
-        ]);
+    $insert->execute([
+        ':pro_id'    => $pro_id,
+        ':pro_name'       => $pro_name,
+        ':pro_image'  => $pro_image,
+        ':pro_price'  => $pro_price,
+        ':pro_amount'  => $pro_amount,
+        ':pro_file'  => $pro_file,
+        ':user_id'  => $user_id,
+    ]);
 }
 
 
@@ -60,30 +60,31 @@ if (isset($_GET['id'])) {
                         </div>
                         <p class="about"><?php echo $product->description; ?></p>
 
-                        <form action="" method="post" id="form-data">
+                        <form method="post" id="form-data">
                             <div class="">
-                                <input type="text" name="pro_id" class="form-control" value="<?php echo $product->id; ?>" name="email">
+                                <input type="hidden" name="pro_id" class="form-control" value="<?php echo $product->id; ?>">
                             </div>
                             <div class="">
-                                <input type="text" name="pro_name" class="form-control" value="<?php echo $product->name; ?>" name="email">
+                                <input type="hidden" name="pro_name" class="form-control" value="<?php echo $product->name; ?>">
                             </div>
                             <div class="">
-                                <input type="text" name="pro_image" class="form-control" value="<?php echo $product->image; ?>" name="email">
+                                <input type="hidden" name="pro_image" class="form-control" value="<?php echo $product->image; ?>">
                             </div>
                             <div class="">
-                                <input type="text" name="pro_price" class="form-control" value="<?php echo $product->price; ?>" name="email">
+                                <input type="hidden" name="pro_price" class="form-control" value="<?php echo $product->price; ?>">
                             </div>
                             <div class="">
-                                <input type="text" name="pro_amount" class="form-control" value="1" name="email">
+                                <input type="hidden" name="pro_amount" class="form-control" value="1">
                             </div>
                             <div class="">
-                                <input type="text" name="pro_file" class="form-control" value="<?php echo $product->file; ?>" name="email">
+                                <input type="hidden" name="pro_file" class="form-control" value="<?php echo $product->file; ?>">
                             </div>
                             <div class="">
-                                <input type="text" name="user_id" class="form-control" value="<?php echo $_SESSION['user_id']; ?>" name="email">
+                                <input type="hidden" name="user_id" class="form-control" value="<?php echo $_SESSION['user_id']; ?>">
                             </div>
 
                             <div class="cart mt-4 align-items-center">
+                                <button type="submit" name="submit" desablese class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Ajouter au panier</button>
                                 <button type="submit" name="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Ajouter au panier</button>
                             </div>
                         </form>
@@ -98,12 +99,12 @@ if (isset($_GET['id'])) {
 <script>
     $(document).ready(function() {
         $(document).on("submit", function(e) {
-            var formdata = $("#form-data").serialize()+"&submit=submit";
+            var formdata = $("#form-data").serialize() + "&submit=submit";
             $.ajax({
                 type: "post",
                 url: "single.php?id=<?php echo $id; ?>",
                 data: formdata,
-                success: function(){
+                success: function() {
                     alert('ajouté au panier avec succes');
                 }
             });
